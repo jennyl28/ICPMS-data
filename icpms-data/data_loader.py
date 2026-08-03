b403406796ccc083b270394950627dff5727c5c7
jennyl28 <jenny.laird@npl.co.uk>
2026-08-03 11:31:51 +0100

    Implement function to load ICP-MS data from Excel
    Added a function to load ICP-MS data from an Excel file.

--- icpms-data/data_loader.py
+++ icpms-data/data_loader.py
@@ -1,1 +1,12 @@
+import pandas as pd
 
+def load_icpms_file(uploaded_file):
+
+    xls = pd.ExcelFile(uploaded_file)
+
+    raw_data = pd.read_excel(
+        xls,
+        sheet_name=0
+    )
+
+    return raw_data
