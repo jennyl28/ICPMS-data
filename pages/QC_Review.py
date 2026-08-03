89659fdfcb940255aa03da3137c97e4de9bea6cf
jennyl28 <jenny.laird@npl.co.uk>
2026-08-03 16:08:42 +0100

    Update QC_Review.py

--- pages/QC_Review.py
+++ pages/QC_Review.py
@@ -1,24 +1,32 @@
 import streamlit as st
-
-from icpms_data.utils import (
-    build_qc_table
-)
+import pandas as pd
 
 st.title("QC Review")
 
-if "processed_data" in st.session_state:
+if (
+    "calibration_results"
+    in st.session_state
+):
 
-    qc = build_qc_table(
-        st.session_state[
-            "processed_data"
-        ]
-    )
+    qc = []
 
-    st.session_state[
-        "qc_table"
-    ] = qc
+    for (
+        isotope,
+        fit
+    ) in st.session_state[
+        "calibration_results"
+    ].items():
+
+        qc.append({
+            "Isotope": isotope,
+            "Slope": fit["slope"],
+            "Intercept": fit[
+                "intercept"
+            ],
+            "R²": fit["r2"]
+        })
 
     st.dataframe(
-        qc,
+        pd.DataFrame(qc),
         use_container_width=True
     )
