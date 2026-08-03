77c46db96b42d64a4df5c21e6089f0e28dfedcca
jennyl28 <jenny.laird@npl.co.uk>
2026-08-03 16:01:42 +0100

    Update Processed_Data.py

--- pages/Processed_Data.py
+++ pages/Processed_Data.py
@@ -4,46 +4,76 @@ from icpms_data.blank_corrections import (
     apply_nearest_blank
 )
 
-from icpms_data.dilution_corrections import (
-    apply_dilution
-)
-
 from icpms_data.mass_corrections import (
     apply_mass_correction
 )
 
+from icpms_data.dilution_corrections import (
+    apply_dilution
+)
+
+from icpms_data.calibration_processing import (
+    apply_all_calibrations
+)
+
 st.title("Process Data")
 
-if "raw_data" in st.session_state:
+if "raw_data" not in st.session_state:
 
-    df = st.session_state[
-        "raw_data"
-    ].copy()
-
-    isotope = st.text_input(
-        "Isotope Column",
-        "226 - > 226 Ra"
+    st.warning(
+        "Upload ICP-MS file first"
     )
 
-    if st.button("Process"):
+    st.stop()
+
+df = st.session_state[
+    "raw_data"
+].copy()
+
+isotopes = st.session_state[
+    "isotope_info"
+]
+
+if st.button("Process Data"):
+
+    for isotope in isotopes:
+
+        cps_col = isotope["column"]
 
         df = apply_nearest_blank(
             df,
-            isotope
+            cps_col
         )
 
         df = apply_dilution(
             df,
-            isotope
+            cps_col
         )
 
         df = apply_mass_correction(
             df,
-            isotope
+            cps_col
+        )
+
+    if st.session_state.get(
+        "has_standards",
+        False
+    ):
+
+        (
+            df,
+            calibration_results
+        ) = apply_all_calibrations(
+            df,
+            isotopes
         )
 
         st.session_state[
-            "processed_data"
-        ] = df
+            "calibration_results"
+        ] = calibration_results
 
-        st.dataframe(df)
+    st.session_state[
+        "processed_data"
+    ] = df
+
+    st.dataframe(df)
