6179f6a099bdf7bf4da9c5539c39cade388ac515
jennyl28 <jenny.laird@npl.co.uk>
2026-08-03 11:59:10 +0100

    Implement build_qc_table function in utils.py
    Added a function to build a quality control table from a DataFrame.

--- icpms-data/utils.py
+++ icpms-data/utils.py
@@ -1,1 +1,16 @@
+import pandas as pd
 
+
+def build_qc_table(df):
+
+    qc = df[
+        [
+            "Sample Name",
+            "Applied Blank",
+            "Applied Blank Value",
+            "Dilution Factor",
+            "Mass Correction"
+        ]
+    ].copy()
+
+    return qc
