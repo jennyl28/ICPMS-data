fb0a3695cf0f5d374f984ab42a71eede2b473a7c
jennyl28 <jenny.laird@npl.co.uk>
2026-08-03 16:09:04 +0100

    Update Recovery_Calculations.py

--- pages/Recovery_Calculations.py
+++ pages/Recovery_Calculations.py
@@ -1,9 +1,28 @@
 import streamlit as st
 
 st.title(
-    "Recoveries"
+    "Recovery Calculations"
 )
 
-st.info(
-    "Recoveries calculated for elements"
+if (
+    "processed_data"
+    not in st.session_state
+):
+
+    st.stop()
+
+df = st.session_state[
+    "processed_data"
+]
+
+recovery_cols = [
+    col
+    for col
+    in df.columns
+    if "_mass_corr" in col
+]
+
+st.write(
+    f"{len(recovery_cols)} "
+    "processed isotope channels"
 )
