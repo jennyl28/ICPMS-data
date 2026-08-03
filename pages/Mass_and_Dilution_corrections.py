4c721646bd769a559f4c9c5795dd6c31460dd27a
jennyl28 <jenny.laird@npl.co.uk>
2026-08-03 12:01:27 +0100

    Implement Streamlit UI for mass and dilution factors
    Added Streamlit integration for mass and dilution factors display.

--- pages/Mass_and_Dilution_corrections.py
+++ pages/Mass_and_Dilution_corrections.py
@@ -1,1 +1,19 @@
+import streamlit as st
 
+st.title(
+    "Mass & Dilution Factors"
+)
+
+if "mass_data" not in st.session_state:
+
+    st.warning(
+        "Upload Mass File First"
+    )
+
+else:
+
+    st.write(
+        st.session_state[
+            "mass_data"
+        ]
+    )
