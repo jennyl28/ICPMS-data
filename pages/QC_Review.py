65150a00cf5b013cb6aed71fec953d24ffabfb86
jennyl28 <jenny.laird@npl.co.uk>
2026-08-03 12:02:54 +0100

    Add QC Review page with data display

--- pages/QC_Review.py
+++ pages/QC_Review.py
@@ -1,1 +1,24 @@
+import streamlit as st
 
+from icpms_data.utils import (
+    build_qc_table
+)
+
+st.title("QC Review")
+
+if "processed_data" in st.session_state:
+
+    qc = build_qc_table(
+        st.session_state[
+            "processed_data"
+        ]
+    )
+
+    st.session_state[
+        "qc_table"
+    ] = qc
+
+    st.dataframe(
+        qc,
+        use_container_width=True
+    )
