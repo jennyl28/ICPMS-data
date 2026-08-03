bb90c700ee52e3c9c92e5e1ec39bbdad4e9c0ddc
jennyl28 <jenny.laird@npl.co.uk>
2026-08-03 12:04:26 +0100

    Add export functionality for processed data

--- pages/Export_Results.py
+++ pages/Export_Results.py
@@ -1,1 +1,25 @@
+import streamlit as st
 
+from icpms_data.exports import (
+    export_excel
+)
+
+st.title("Export")
+
+if (
+    "processed_data"
+    in st.session_state
+):
+
+    excel_data = export_excel(
+        st.session_state[
+            "processed_data"
+        ]
+    )
+
+    st.download_button(
+        "Download Results",
+        excel_data,
+        "Processed_Results.xlsx",
+        mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    )
