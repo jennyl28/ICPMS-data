0fe7d23f536714ee6dddd75159af7173eb1b3bda
jennyl28 <jenny.laird@npl.co.uk>
2026-08-03 11:32:14 +0100

    Add function to load mass file from Excel

--- icpms-data/mass_parser.py
+++ icpms-data/mass_parser.py
@@ -1,1 +1,10 @@
+import pandas as pd
 
+def load_mass_file(uploaded_file):
+
+    xls = pd.ExcelFile(uploaded_file)
+
+    return {
+        sheet: pd.read_excel(xls, sheet)
+        for sheet in xls.sheet_names
+    }
