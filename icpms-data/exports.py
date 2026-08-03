77e8d2450cb643112dc46d7cc3e955c44b3f4dec
jennyl28 <jenny.laird@npl.co.uk>
2026-08-03 12:00:33 +0100

    Implement export_excel function for Excel export
    Add function to export results to Excel format.

--- icpms-data/exports.py
+++ icpms-data/exports.py
@@ -1,1 +1,20 @@
+from io import BytesIO
+import pandas as pd
 
+
+def export_excel(results):
+
+    output = BytesIO()
+
+    with pd.ExcelWriter(
+        output,
+        engine="xlsxwriter"
+    ) as writer:
+
+        results.to_excel(
+            writer,
+            sheet_name="Results",
+            index=False
+        )
+
+    return output.getvalue()
