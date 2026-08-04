c30721f3f41622ecf4a9f8ff9970afa859550d89
jennyl28 <jenny.laird@npl.co.uk>
2026-08-04 16:43:50 +0100

    Refactor load_icpms_file to use uploaded_file directly

--- icpms_data/data_loader.py
+++ icpms_data/data_loader.py
@@ -2,11 +2,10 @@ import pandas as pd
 
 def load_icpms_file(uploaded_file):
 
-    xls = pd.ExcelFile(uploaded_file)
-
-    raw_data = pd.read_excel(
-        xls,
-        sheet_name=0
+    df = pd.read_excel(
+        uploaded_file,
+        sheet_name=0,
+        header=None
     )
 
-    return raw_data
+    return df
