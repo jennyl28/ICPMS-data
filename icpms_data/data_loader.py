735adcbcfd9c4528d019fd50c197cb627f2b2f8c
jennyl28 <jenny.laird@npl.co.uk>
2026-08-04 17:19:53 +0100

    Update data_loader.py

--- icpms_data/data_loader.py
+++ icpms_data/data_loader.py
@@ -4,23 +4,15 @@ def load_icpms_file(uploaded_file):
 
     raw = pd.read_excel(
         uploaded_file,
-        sheet_name=0,
         header=None
     )
 
-    raw = raw.T
+    raw = raw.set_index(0)
 
-    raw.columns = raw.iloc[0]
+    df = raw.T
 
-    raw = raw.iloc[1:].reset_index(
+    df = df.reset_index(
         drop=True
     )
-    st.write("Shape")
-    st.write(df.shape)
-    
-    st.write("Top left corner")
-    
-    st.dataframe(
-        df.iloc[:20, :20])
 
-    return raw
+    return df
