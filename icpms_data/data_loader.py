37a93e47a77609361fa41852cb5f38ebf481914a
jennyl28 <jenny.laird@npl.co.uk>
2026-08-04 17:25:33 +0100

    Refactor data loading in load_icpms_file function
    Refactor load_icpms_file function to streamline data loading.

--- icpms_data/data_loader.py
+++ icpms_data/data_loader.py
@@ -2,17 +2,11 @@ import pandas as pd
 
 def load_icpms_file(uploaded_file):
 
-    raw = pd.read_excel(
+    df = pd.read_excel(
         uploaded_file,
+        sheet_name=0,
         header=None
     )
 
-    raw = raw.set_index(0)
-
-    df = raw.T
-
-    df = df.reset_index(
-        drop=True
-    )
-
     return df
+
