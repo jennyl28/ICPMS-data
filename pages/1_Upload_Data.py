37a177d08302bc7190144460c980faa551c286d9
jennyl28 <jenny.laird@npl.co.uk>
2026-08-04 16:26:03 +0100

    Update 1_Upload_Data.py

--- pages/1_Upload_Data.py
+++ pages/1_Upload_Data.py
@@ -52,6 +52,6 @@ if mass_file:
     )
 
 if has_stds:
-    st.info(session_state["Calibration standards will be extracted from ICP-MS file")
+    st.info(session_state["Calibration standards will be extracted from ICP-MS file"])
 
    
